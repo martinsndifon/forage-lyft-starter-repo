f7ca754dcbbd6eb992c6172456061f84437d6b95
martinsndifon <martinsndifon@gmail.com>
2023-10-17 18:40:40 +0100

    complete task 3 - writing unittest

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,11 +1,11 @@
-from battery import Battery
+from battery.battery import Battery
 from utils import add_years_to_date
 
 
 class SpindlerBattery(Battery):
     """Defines the Spindler battery"""
 
-    def __init__(self, last_service_date, current_date):
+    def __init__(self, current_date, last_service_date):
         """Initialize the class"""
         self.last_service_date = last_service_date
         self.current_date = current_date
